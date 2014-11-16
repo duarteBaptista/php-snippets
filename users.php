046550ab8bd35ff7798a40f39d3a44ab145f199a
Julian Köhn <julian@maxgolds.com>
2014-11-16 17:57:06 +0100

    Created users.php basefile

---
+++ users.php
@@ -0,0 +1,39 @@
+<?php
+	/**
+	 * 	User Management
+	 * 	Registration
+	 * 	Login
+	 *	Profiles
+	 *	Remove User
+	 *	Update User
+	 */
+
+	/* SQL Structure:
+
+	
+	*/	 
+	
+class user 
+{
+	public function register()
+	{
+	
+	}
+	
+	public function login()
+	{
+	
+	}
+	
+	public function update()
+	{
+	
+	}
+	
+	public function delete()
+	{
+	
+	}
+}
+
+?>
